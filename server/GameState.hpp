2c50bbca152e48bec289a3597330f5d1b3f1847f
Gabe Decker <42gdeckers@gmail.com>
2025-07-30 20:21:54 -0600

    Possibly done with GameState object, having also added a hpp file

--- server/GameState.hpp
+++ server/GameState.hpp
@@ -1,11 +1,28 @@
 #pragma once
 
+#include <array>
+#include <cstdint>
+#include <string>
+
 using Board = std::array<std::array<std::string, 3>, 3>;
 class GameState
 {
   public:
+    void GameState(Board board, std::uint8_t redID, std::uint8_t blueID, bool isRedTurn);
+
+    Board getBoard() { return m_board }
+    std::uint8_t getRedID() { return m_redID }
+    std::uint8_t getBlueID() { return m_blueID }
+    bool getIsRedTurn() { return m_isRedTurn }
+
+    void setBoard(Board board) { m_board = board }
+    void setRedID(std::uint8_t redID) { m_redID = redID }
+    void setBlueID(std::uint8_t blueID) { m_blueID = blueID }
+    void setIsRedTurn(bool isRedTurn) { m_isRedTurn = isRedTurn }
+
   private:
-    Board board;
-    std::uint8_t redID;
-    std::uint8_t blueID;
+    Board m_board;
+    std::uint8_t m_redID;
+    std::uint8_t m_blueID;
+    bool m_isRedTurn;
 }
