cfcdbfbef4089729d54aaaec4bd9dff07732d424
Gabe Decker <42gdeckers@gmail.com>
2025-12-30 22:05:55 -0700

    Changed the function to get client info so that it actually gets the name and whether the client wants to host a match

--- server/matchmaking.cpp
+++ server/matchmaking.cpp
@@ -5,19 +5,24 @@
 #include <cstdint>
 #include <tuple>
 
-// TODO: Update to decide where/how ID is determined
 std::tuple<Player, bool, bool> matchmaking::getClientInfo(int client_fd, std::uint8_t client_id)
 {
     bool disconnected = false;
-    bool isHosting = true;  // TODO: determine this
     const int clientInfoBufferLen = 20;
     char clientInfoBuffer[clientInfoBufferLen];
     int numbytes = networking::receiveAll(client_fd, clientInfoBuffer, clientInfoBufferLen);
+
+    // First char signifies whether client wants to host a game.
+    // The other chars are the name the client picks
+    // TODO: someday add limits to the names and check the current names to make sure it's unique
+    std::string clientInfo(clientInfo);
+    bool isHosting = clientInfo.at(0) == 'H';
+    std::string client_name = clientInfo.substr(1);
     if (numbytes == -1 || numbytes == 0)
     {
         disconnected = true;
     }
     // TODO: Update how to get player name
-    Player player = Player("update me", client_id, client_fd);
+    Player player = Player(client_name, client_id, client_fd);
     return std::make_tuple(player, disconnected, isHosting);
 }
