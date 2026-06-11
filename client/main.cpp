66ef82285b3a37a20a94ee52dc81465e15b41827
Gabe Decker <42gdeckers@gmail.com>
2026-06-10 22:40:00 -0600

    Added test code for the client to test sending info to server.

--- client/main.cpp
+++ client/main.cpp
@@ -1,10 +1,24 @@
 #include "../game/interface.hpp"
 #include "../game/ttt.hpp"
+#include "matchmaking.hpp"
 #include "networking.hpp"
+
+#include <print>
+#include <string>
 
 void doMultiplayer()
 {
+    // TODO: get code to get username, using character limit
+    // and whether they want to host or join a lobby
+    std::string username = "tmpuser1234567"; // should be max char limit
+    bool hostGame = true;
     int serv_fd = networking::initClient();
+
+    bool disconnected;
+    disconnected = matchmaking::sendPlayerInfo(serv_fd, hostGame, username);
+    std::print("Sent player info");
+
+    networking::closeFd(serv_fd);
 }
 
 int main()
